71d190a506c1919818f002728669aa7ad3f13716
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-09-15 16:43:40 -0700

    Checker now runs test suites and displays final results. Moved all test suites from checker.sh into tests/

--- checker.sh
+++ checker.sh
@@ -1,247 +1,90 @@
 #!/bin/bash
 
-echo "Basic instruction tests"
+source colors.sh
 
-# live
-./test.sh -c 14,15 "01 FF FF FF FF 03 70 01"
-# ld
-./test.sh -c 9,10 "02 90 89 AB CD EF 01 03 70 01"
-./test.sh -c 9,10 "02 D0 2A 01 10 03 70 10"
-# st
-./test.sh -c 4,5 "03 70 01 42 02"
-./test.sh -c 9,10 "03 50 01 10 03 70 10"
-# add
-./test.sh -c 14,15 "04 54 01 01 10 03 70 10"
-./test.sh -c 24,25 "02 90 F2 97 46 D5 0E 02 90 37 4C 6A 75 03 04 54 0E 03 03 03 70 03"
-# sub
-./test.sh -c 14,15 "05 54 10 01 10 03 70 10"
-./test.sh -c 24,25 "02 90 2F 97 46 D5 0E 02 90 37 4C 6A 75 03 05 54 0E 03 03 03 70 03"
-# and
-./test.sh -c 20,21 "02 90 A9 D1 99 5B 07 02 90 7B CE 09 A1 06 06 54 07 06 0D 03 70 0D"
-./test.sh -c 10,11 "06 A4 C6 32 26 63 C3 DF 90 2D 04 03 70 04"
-./test.sh -c 10,11 "06 F4 18 01 4C 0C 0F 03 70 0F 00 00 94 71 9D 94"
-# or
-./test.sh -c 15,16 "02 90 CE 8F 8C 0B 03 07 64 03 91 64 AF DD 06 03 70 06"
-./test.sh -c 10,11 "07 B4 39 A3 87 28 74 0E 0D 03 70 0D 00 00 CF C4 C8 3E"
-./test.sh -c 15,16 "02 90 39 0E 3A 5D 0A 07 D4 AF FF 0A 0E 03 70 0E"
-# xor
-./test.sh -c 15,16 "02 90 FF 95 00 52 05 08 74 05 F5 F9 05 03 70 05"
-./test.sh -c 15,16 "02 90 EB DB 88 BE 06 08 94 AD 32 1B C8 06 0A 03 70 0A"
-./test.sh -c 10,11 "08 E4 08 0E C6 D4 15 0F 0E 03 70 0E 00 00 A4 4E 1C 7D"
-# zjmp
-./test.sh -c 29,30 "02 90 00 00 00 00 10 09 00 04 0C 03 70 01"
-./test.sh -c 25 "09 00 04 00 03 70 01"
-./test.sh -c 39,40 "02 90 03 70 01 00 07 02 D0 00 64 08 03 70 07 00 0F 09 00 0A"
-# ldi
-./test.sh -c 29,30 "0A 54 01 01 01 03 70 01"
-./test.sh -c 34,35 "02 90 FF FF E8 A8 0E 0A 64 0E 17 51 08 03 70 08"
-./test.sh -c 34,35 "02 90 74 19 98 01 06 0A 94 96 02 06 0B 03 70 0B"
-./test.sh -c 29,30 "0A A4 57 BB D2 51 0C 03 70 0C 00 00 54 CD 1D 6B"
-./test.sh -c 29,30 "0A D4 6E 05 01 0C 03 70 0C 00 00 D3 80 C8 EB"
-./test.sh -c 29,30 "0A E4 97 FD FF F8 07 03 70 07"
-# sti
-./test.sh -c 24,25 "0B 54 01 01 01"
-./test.sh -c 29,30 "02 90 FF FF E8 A8 0E 0B 58 01 0E 17 51"
-./test.sh -c 29,30 "02 90 74 19 98 01 06 0B 64 06 96 02 06"
-./test.sh -c 24,25 "0B 68 01 57 BB D2 51"
-./test.sh -c 24,25 "0B 74 01 6E 04 01 4C 09"
-./test.sh -c 24,25 "0B 78 01 97 FD FF F8"
-# fork
-./test.sh -c 804,805 "0C 00 08 03 70 10 FF FE 03 70 01"
-./test.sh -c 824,825 "09 03 70 0C 99 FE 03 70 01"
-./test.sh -c 809,810 "02 90 12 34 56 78 10 0C 00 08 03 70 10 00 00 03 70 10 00 00"
-./test.sh -c 829,830 "02 D0 00 64 01 0C 00 06 09 00 06 09 00 08 03 70 01 00 00 03 70 01"
-./test.sh -c 1614,1615 "01 FF FF FF FF 0C 00 0E 0C 00 08 03 70 01 00 00 03 70 01 0C 00 08 03 70 01 00 00 03 70 01"
-# lld
-./test.sh -c 14,15 "0D 90 89 AB CD EF 01 03 70 01"
-# okay so apparently lld loads a short for whatever reason when called with an ind arg
-./test.sh -c 14,15 "0D D0 00 01 10 03 70 10"
-./test.sh -c 14,15 "0D D0 00 02 10 03 70 10"
-./test.sh -c 24,25 "02 90 1F 90 6F 8D 0F 03 70 0F FE 01 0D D0 FD FC 03 03 70 03"
-# lldi
-./test.sh -c 64,65 "02 90 CF 0B FE FE 04 03 70 04 FE 01 0E 54 04 04 01 03 70 01"
-./test.sh -c 59,60 "02 90 FF FF E8 A8 0E 0E 64 0E 17 51 08 03 70 08"
-./test.sh -c 59,60 "02 90 E4 19 00 01 06 0E 94 00 02 06 0B 03 70 0B"
-./test.sh -c 54,55 "0E A4 57 BB A8 51 0C 03 70 0C 00 00 54 CD 1D 6B"
-./test.sh -c 54,55 "0E D4 97 FD 01 0C 03 70 0C 00 00 00 00 D3 80 C8 EB"
-./test.sh -c 64,65 "02 90 FF FF 7E 2A 10 03 70 10 FE 01 0E E4 FF F6 7F D2 07 03 70 07"
-# lfork
-./test.sh -c 1024,1025 "09 03 70 0F FF FE 03 70 01"
-./test.sh -c 1009,1010 "02 90 12 34 56 78 10 0F 00 08 03 70 10 00 00 03 70 10 00 00"
-./test.sh -c 1029,1030 "02 D0 00 64 01 0F 00 06 09 00 06 09 00 08 03 70 01 00 00 03 70 01"
-./test.sh -c 2014,2015 "01 FF FF FF FF 0F 00 0E 0F 00 08 03 70 01 00 00 03 70 01 0F 00 08 03 70 01 00 00 03 70 01"
-./test.sh -c 1009,1010 "0F 0F F4 02 90 03 70 01 00 09 03 70 09 FF F5"
-./test.sh -c 1015,1016 "0F 0F F4 02 90 03 70 01 00 09 03 70 09 FF F5"
-# aff
-./test.sh -c 6,7 "10 40 01 03 70 01"
+function pad_string
+{
+	str=$1
+	if (( ${#str} > $2 ))
+	then
+		str="${1:0:$(($2 - 3))}..."
+	fi
+	if (( ($2 - ${#str}) % 2 ))
+	then
+		str="$str "
+	fi
+	while (( ${#str} < $2 ))
+	do
+		str="$str ."
+	done
+	printf "$str"
+}
 
+function test_suite
+{
+	if [ -z "$1" ] || [ -z "$2" ] ; then
+		echo "Skipping badly formatted test suite."
+		return 1
+	fi
 
+	echo "=== ${t_bold}Test suite \"$1\"${t_off} ================================="
+	if [ -z "$num_total" ] ; then
+		num_total=0
+	fi
+	if [ -z "$num_failed" ] ; then
+		num_failed=0
+	fi
+	suite_num_total=0
+	suite_num_failed=0
 
-echo "Carry flag tests"
+	let "run_str_width = 40 + ${#1}"
+	while read test; do
+		if [[ $test != "./test.sh "* ]] ; then
+			continue
+		fi
+		run_str=$(pad_string "Running test ${test#./test.sh }" $run_str_width)
+		printf "    $run_str "
+		eval $test > /dev/null
+		if (( ${PIPESTATUS[0]} )) ; then
+			printf "${c_red}KO${c_off}\n"
+			failed_tests="$failed_tests$test"$'\n'
+			let "suite_num_failed++"
+			let "num_failed++"
+		else
+			printf "${c_green}OK${c_off}\n"
+		fi
+		let "num_total++"
+		let "suite_num_total++"
+	done < <(echo "$2")
 
-# carry-modifying instructions
-# ld
-./test.sh -c 30,54,55 "02 90 00 00 00 00 10 09 00 05 03 70 02 90 80 00 00 00 10 09 00 06 03 70 01"
-./test.sh -c 30,54,55 "02 D0 00 64 10 09 00 05 03 70 02 D0 00 09 10 09 00 06 03 70 01"
-# add/sub
-./test.sh -c 35,64,65 "04 54 02 02 02 09 00 05 03 70 04 54 01 01 01 09 00 06 03 70 01"
-./test.sh -c 35,64,65 "05 54 02 02 02 09 00 05 03 70 05 54 01 02 01 09 00 06 03 70 01"
-# and/or/xor
-./test.sh -c 31,56,57 "06 B4 00 00 FF FF 00 00 04 09 00 05 03 70 06 D4 00 0B 01 01 09 00 06 03 70 01"
-./test.sh -c 31,56,57 "07 74 06 FF FC 06 09 00 05 03 70 07 E4 00 04 80 00 00 00 07 09 00 06 03 70 07"
-./test.sh -c 31,56,57 "08 54 06 0D 10 09 00 05 03 70 08 A4 89 AB CD EF 09 AB CD EF 04 09 00 06 03 70 04"
-# lld/lldi
-./test.sh -c 35,64,65 "0D 90 00 00 00 00 10 09 00 05 03 70 0D 90 80 00 00 00 10 09 00 06 03 70 01"
-./test.sh -c 35,64,65 "0D D0 00 64 10 09 00 05 03 70 0D D0 00 09 10 09 00 06 03 70 01"
-./test.sh -c 34,35 "0D D0 FF FE 10 09 00 04 0C 03 70 01"
-./test.sh -c 75,144,145 "0E A4 FF FE FF FE 02 09 00 05 03 70 0E 54 03 06 0B 09 00 06 03 70 01"
-./test.sh -c 74,75 "0E D4 00 01 04 0D 09 00 04 0C 03 70 01"
+	printf "    "
+	echo "$1-------------------------------------------" | sed "s/./-/g"
 
-# non-modifying instructions (carry 0)
-./test.sh -c 34,35 "01 00 00 00 00 09 00 06 03 70 01"
-./test.sh -c 29,30 "03 70 09 00 00 09 00 06 03 70 01"
-./test.sh -c 44,45 "09 00 00 09 00 06 03 70 01"
-./test.sh -c 49,50 "0A 94 FF FC 09 06 09 00 06 03 70 01"
-./test.sh -c 49,50 "0B 78 06 00 00 00 00 09 00 06 03 70 01"
-./test.sh -c 824,825 "0C 00 00 09 00 06 03 70 01"
-./test.sh -c 1024,1025 "0F 00 00 09 00 06 03 70 01"
-./test.sh -c 26,27 "10 40 08 09 00 06 03 70 01"
+	echo "    $suite_num_failed tests failed out of $suite_num_total total tests."
 
-# non-modifying instructions (carry 1)
-./test.sh -c 39,40 "02 D0 00 64 01 01 00 00 00 00 09 00 04 0C 03 70 01"
-./test.sh -c 34,35 "02 D0 00 64 05 03 50 01 0F 09 00 04 0C 03 70 01"
-./test.sh -c 49,50 "02 D0 00 64 05 09 00 04 0C 09 00 04 0C 03 70 01"
-./test.sh -c 54,55 "02 D0 00 64 05 0A 64 08 FF FF 07 09 00 04 0C 03 70 01"
-./test.sh -c 54,55 "02 D0 00 64 05 0B 74 01 00 64 0E 09 00 04 0C 03 70 01"
-./test.sh -c 829,830 "02 D0 00 64 05 0C 00 64 09 00 04 0C 03 70 01"
-./test.sh -c 1029,1030 "02 D0 00 64 05 0F 00 64 09 00 04 0C 03 70 01"
-./test.sh -c 31,32 "02 D0 00 64 05 10 40 01 09 00 04 0C 03 70 01"
+	echo "$1===================================================" | sed "s/./=/g"
+	echo
+}
 
+export inf_loop="02 D0 00 64 10 09 00 00"
 
-
-echo "Live tests"
-
-inf_loop="02 D0 00 64 10 09 00 00"
-./test.sh "01 FF FF FF FF $inf_loop" "$inf_loop"
-./test.sh "$inf_loop" "01 FF FF FF FE $inf_loop"
-./test.sh "01 FF FF FF FE $inf_loop" "$inf_loop"
-./test.sh "$inf_loop" "01 FF FF FF FF $inf_loop"
-./test.sh "01 FF FF FF FF 01 FF FF FF FF $inf_loop" \
-          "01 FF FF FF FE $inf_loop" \
-		  "09 00 03 01 FF FF FF FD $inf_loop" \
-		  "04 54 02 02 02 01 FF FF FF FC $inf_loop"
-./test.sh "01 FF FF FF FF 01 FF FF FF FE 01 FF FF FF FD 01 FF FF FF FC $inf_loop" "$inf_loop"
-./test.sh ""
-./test.sh "" ""
-./test.sh "" "" ""
-./test.sh "" "" "" ""
-./test.sh "$inf_loop" "$inf_loop" "$inf_loop" \
-          "01 FF FF FF FB 01 7F FF FF FF 01 BF FF FF FF $inf_loop"
-./test.sh "01 00 00 00 00 01 00 00 00 01 01 00 00 00 02 01 93 39 D4 CA $inf_loop" \
-          "$inf_loop" "$inf_loop" "$inf_loop"
-./test.sh -c 1755,1756 "01 FF FF FF FC 02 90 00 00 00 01 02 04 54 02 02 02 06 64 02 80 00 00 00 03 09 00 03 09 FF F0 03 70 01"
-./test.sh -c 1755,1756 "01 00 00 00 00 02 90 00 00 00 01 02 04 54 02 02 02 06 64 02 80 00 00 00 03 09 00 03 09 FF F0 03 70 01"
-./test.sh -c 1755,1756 "01 65 F9 91 92 02 90 00 00 00 01 02 04 54 02 02 02 06 64 02 80 00 00 00 03 09 00 03 09 FF F0 03 70 01"
-./test.sh -c 3016,3017,3022,3023 "02 90 00 00 00 01 02 01 00 00 00 00 01 FF FF FF FB 01 A6 4D 66 4F 04 54 02 02 02 06 64 02 00 80 00 00 03 09 FF E4 02 90 00 00 00 01 02 04 54 02 02 02 06 64 02 00 08 00 00 03 09 00 03 09 00 03 09 FF ED 09 00 03 09 00 03 03 70 01 00 00 00 03 70 01" "02 D0 00 64 10 01 00 00 00 00 09 FF FB"
-
-
-
-echo "CYCLE_TO_DIE tests"
-
-./test.sh -c 1535,1536 "02 90 00 00 00 31 02 04 54 03 02 03 06 64 03 00 00 08 00 04 09 FF F3 04 54 03 03 03 10 40 03 10 40 03 03 70 01" "02 D0 00 64 10 01 00 00 00 00 09 FF FB"
-./test.sh -c 1531,1532,1537 "02 90 00 00 00 31 02 04 54 03 02 03 06 64 03 00 00 08 00 04 09 FF F3 04 54 03 03 03 03 70 01 00 00 03 70 01 00 00" "02 D0 00 64 10 01 00 00 00 00 09 FF FB"
-./test.sh -c 1521,1522,1541 "02 90 00 00 00 31 02 04 54 03 02 03 06 64 03 00 00 08 00 04 09 FF F3 03 70 02 00 00 10 40 02 10 40 02 01 FF FF FF FF 03 70 01" "02 D0 00 64 10 01 00 00 00 00 09 FF FB"
-./test.sh -c 1526,1527,1542 "02 90 00 00 00 31 02 04 54 03 02 03 06 64 03 00 00 08 00 04 09 FF F3 03 70 02 00 00 03 70 02 00 00 01 FF FF FF FF 03 70 01" "02 D0 00 64 10 01 00 00 00 00 09 FF FB"
-./test.sh -c 3023,3024 "02 90 00 00 00 01 02 01 FF FF FF FF 0E 54 0F 0F 10 0E 54 0F 0F 10 04 54 03 02 03 06 64 03 00 00 00 08 10 09 FF E4 02 90 00 00 00 0D 03 04 54 03 02 03 06 64 03 00 00 00 40 04 09 FF F3 03 70 01 00 00 03 70 01" "01 FF FF FF FE 0E 54 0F 0F 10 0E 64 0F 00 64 10 09 FF F0"
-./test.sh -c 3018,3019,3024 "02 90 00 00 00 01 02 01 FF FF FF FF 0E 54 0F 0F 10 0E 54 0F 0F 10 04 54 03 02 03 06 64 03 00 00 00 08 10 09 FF E4 02 90 00 00 00 0D 03 04 54 03 02 03 06 64 03 00 00 00 40 04 09 FF F3 03 70 01 00 00 03 70 01" "01 FF FF FF FE 01 FF FF FF FE 0E 54 0F 0F 10 0E 64 0F 00 64 10 09 FF F0"
-./test.sh -c 16844,16845,16850 "02 90 00 00 00 01 02 02 90 00 00 00 33 03 01 FF FF FF FF 0E 54 01 01 10 0E 54 01 01 10 0E 54 01 01 10 04 54 03 02 03 06 64 03 00 00 00 80 10 09 FF DF 02 90 00 00 00 10 03 04 54 03 02 03 06 64 03 00 00 00 40 10 09 FF F3 03 70 01 00 00 03 70 01 00 00 03 70 01" "01 FF FF FF FE 01 FF FF FF FE 0E 54 0F 0F 10 0E 64 0F 00 64 10 09 FF F0"
-
-
-
-echo "Program order tests"
-
-./test.sh -c 4,5 "03 70 02 01 FF" "03 70 01 FE 01" "03 70 01 01 FF" "03 70 01 FE 01"
-./test.sh -c 4,5 "03 70 02 FE 01" "03 70 01 01 FF" "03 70 01 FE 01" "03 70 01 01 FF"
-./test.sh -c 804,805 "0C 00 08 03 70 01 FF FC 03 70 02 FF F7"
-./test.sh -c 1614,1615 "01 00 00 00 00 0C 00 10 0C 00 08 03 70 01 00 1D 03 70 02 00 12 0C 00 08 03 70 02 00 0E 03 70 01 00 07"
-
-
-
-echo "Invalid instruction tests"
-
-# bad op byte
-./test.sh -c 5,6 "00 03 70 01"
-./test.sh -c 5,6 "FF 03 70 01"
-./test.sh -c 5,6 "10 03 70 01"
-./test.sh -c 14,15 "00 11 21 80 81 F1 B0 A3 AE FF 03 70 01"
-
-# bad register number
-./test.sh -c 9,10 "02 D0 00 00 00 03 70 01"
-./test.sh -c 9,10 "02 D0 00 00 11 03 70 01"
-./test.sh -c 9,10 "03 70 00 00 00 03 70 01"
-./test.sh -c 9,10 "03 50 11 01 03 70 01"
-./test.sh -c 14,15 "04 54 00 02 01 03 70 01"
-./test.sh -c 14,15 "04 54 01 11 01 03 70 01"
-./test.sh -c 14,15 "04 54 01 01 41 03 70 01"
-./test.sh -c 14,15 "05 54 00 02 01 03 70 01"
-./test.sh -c 14,15 "05 54 01 11 01 03 70 01"
-./test.sh -c 14,15 "05 54 01 01 41 03 70 01"
-./test.sh -c 10,11 "06 74 00 00 00 01 03 70 01"
-./test.sh -c 10,11 "06 94 00 00 00 00 11 01 03 70 01"
-./test.sh -c 10,11,36 "06 E4 FF FC 00 00 00 00 41 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 10,11 "07 54 11 0F 01 03 70 01"
-./test.sh -c 10,11 "07 94 00 00 00 00 41 01 03 70 01"
-./test.sh -c 10,11,36 "07 A4 00 00 00 00 00 00 00 00 00 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 10,11 "08 64 41 12 34 56 78 01 03 70 01"
-./test.sh -c 10,11 "08 D4 00 00 00 01 03 70 01"
-./test.sh -c 10,11,36 "08 B4 00 00 00 00 FF FC 11 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 29,30 "0A 64 41 00 00 01 03 70 01"
-./test.sh -c 29,30 "0A D4 00 00 11 01 03 70 01"
-./test.sh -c 29,30,55 "0A A4 00 00 FF FC 00 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 29,30 "0B 68 00 00 FF FC 00 03 70 01"
-./test.sh -c 29,30 "0B 58 01 11 00 00 03 70 01"
-./test.sh -c 29,30 "0B 74 01 00 00 41 03 70 01"
-./test.sh -c 14,15 "0D D0 00 00 00 03 70 01"
-./test.sh -c 14,15,40 "0D 90 00 00 00 00 11 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 54,55 "0E 64 41 00 00 01 03 70 01"
-./test.sh -c 54,55 "0E D4 00 00 11 01 03 70 01"
-./test.sh -c 54,55,80 "0E A4 00 00 FF FC 00 03 70 01 00 00 09 00 06 03 70 01"
-./test.sh -c 6,7 "10 40 00 03 70 01"
-./test.sh -c 6,7 "10 40 11 03 70 01"
-
-# bad ACB
-./test.sh -c 6,7 "10 00 03 70 01"
-./test.sh -c 30,31 "06 00 09 0A 0F 03 70 0F"
-./test.sh -c 10,11 "06 50 09 0A 03 70 03"
-./test.sh -c 10,11 "06 55 09 0A 0F 03 70 0F"
-./test.sh -c 29,30 "0B 6C 01 00 00 00 00 03 70 01"
-./test.sh -c 29,30 "0B 5C 09 0A 00 00 03 70 01"
-./test.sh -c 49,50 "0A 78 02 02 02 02 02 09 00 06 03 70 02"
-./test.sh -c 6,7 "10 AA 00 00 00 00 03 70 01"
-./test.sh -c 6,7 "10 E8 00 00 03 70 01"
-./test.sh -c 54,55 "0E B0 00 00 00 00 03 70 01"
-./test.sh -c 34,35 "05 AA 03 03 03 03 03 03 03 03 03 03 03 03 09 00 06 03 70 03"
-./test.sh -c 14,15 "05 F5 00 00 00 00 00 03 70 01"
-
-
-
-echo "Concurrent modification tests"
-
-./test.sh -c 809,810,815 "02 D0 00 09 08 0C 00 04 01 03 70 08 FF FF"
-./test.sh -c 804,805 "0C 00 05 03 70 03 70 01 00 03"
-./test.sh -c 804,805 "0C 00 05 03 70 03 70 01"
-./test.sh -c 809,810 "02 D0 00 04 08 0C 00 05 03 70 03 70 08"
-./test.sh -c 9,10,14,15 "02 90 03 70 01 00 08 03 70 08 00 05 0C"
-./test.sh -c 809,810,815 "02 D0 00 0A 08 0C 00 09 03 70 03 70 01 0C 03 70 08 FF FF"
-./test.sh -c 809,810,815 "02 D0 00 0D 08 0C 00 08 03 70 08 00 0A 03 70 01 00 00 0C"
-./test.sh -c 809,810,815 "02 90 0C 03 70 01 08 0C 00 08 03 70 08 00 09"
-./test.sh -c 815,816,820 "02 90 03 70 01 00 08 0C 00 0C 0D D0 7C 01 08 0C 0C 0C 0C 06 54 02 02 02 03 70 08 FF F7"
-./test.sh -c 815,816,820 "02 90 0C 70 0C 0C 08 0C 00 0D 06 54 06 05 04 03 70 08 00 0A 04 54 01 01 0C 03"
-
-
-
+test_suite "Basic instructions" "$(cat tests/basic.sh)"
+test_suite "Carry flag" "$(cat tests/carry.sh)"
+test_suite "Live" "$(cat tests/live.sh)"
+test_suite "CYCLE_TO_DIE" "$(cat tests/cycle_to_die.sh)"
+test_suite "Process order" "$(cat tests/order.sh)"
+test_suite "Invalid instruction" "$(cat tests/bad_instruction.sh)"
+test_suite "Concurrent modification" "$(cat tests/overwrite.sh)"
 # TODO Simple program tests
-
-
-
+# test_suite "" "$(cat tests/.sh)"
 # TODO Randomly generated tests
+# test_suite "" "$(cat tests/.sh)"
+
+echo "$num_failed tests failed out of $num_total total tests."
+if (( $num_failed ))
+then
+	printf "${c_red}${t_bold}Failed tests:${t_off}${c_off}\n"
+	printf "$failed_tests" | sed "s/^/    /"
+else
+	printf "${c_green}All tests passed!${c_off}\n"
+fi
